a082f8e9cc03b0b3ee264b7271daf6b14832e2b6
bonapartbon <bonapathb1@gmail.com>
2021-09-29 21:50:36 +0700

    hyperx

--- src/components/json-items/hyper-x.js
+++ src/components/json-items/hyper-x.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
-class logitech extends Component {
+class hyperx extends Component {
 
     constructor(props) {
         super(props);
@@ -11,44 +11,44 @@ class logitech extends Component {
     }
 
     componentDidMount() {
-        fetch('https://my-json-server.typicode.com/bonapartbon/pro-accessories/logitech')
-            .then(res => res.json())
-            .then(json => {
-                this.setState({
-                    isLoaded: true,
-                    items: json,
-                })
-            });
+        fetch('https://my-json-server.typicode.com/bonapartbon/pro-accessories/hyper-x')
+        .then(res => res.json())
+        .then(json => {
+            this.setState({
+                isLoaded: true,
+                items: json,
+            })
+        });
     }
 
     render() {
-
+        
         var { isLoaded, items } = this.state;
 
-        if (!isLoaded) {
+        if(!isLoaded) {
             return <div>Loading...</div>;
         }
         else {
-            return (
+            return(
                 <div>
                     <div className="container brand-tag border border-bottom-0 brand-div">
-                        <img src="assets/img/logitech-logo.png" alt="" className="ml-2 mt-2 float-left logo1" width="15%" />
+                        <img src="assets/img/hyper-x-logo.png" alt="" className="ml-2 mt-2 float-left logo1" width="15%"/>
                     </div>
                     <div className="container home-container">
                         <div className="row text-center border">
-                            {items.map(item => (
+                            {items.map(item => (           
                                 <div className="col-3 border p-1" key={item.id}>
                                     <img src={item.img} alt="" width="100%" />
                                     <p className="pt-2 item-des">{item.name}</p>
-                                    <p className="mb-2"><strong>${item.price}</strong></p>
+                                    <p className="mb-2"><strong>${item.price}</strong></p>              
                                 </div>
                             ))}
                         </div>
                     </div>
-                </div>
+                </div>      
             );
         }
     }
 }
 
-export default logitech;
+export default hyperx;
